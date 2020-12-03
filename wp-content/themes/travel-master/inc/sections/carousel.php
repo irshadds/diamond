4a997b1cd4012009018e99890c7db19182d2cc09
irshad <irshad@dxd.tech>
2020-12-03 21:27:07 +0530

    Added Primary Menu Overalay

--- wp-content/themes/travel-master/inc/sections/carousel.php
+++ wp-content/themes/travel-master/inc/sections/carousel.php
@@ -1,1 +1,5 @@
 <?php echo do_shortcode('[slide-anything id="83"]'); ?>
+
+<div class="visible-title">
+                                    <h2 class="entry-title"><a href="<?php echo esc_url( $content['url'] ); ?>"><?php echo esc_html( $content['title'] ); ?></a></h2>
+                        </div><!-- .visible-title -->
